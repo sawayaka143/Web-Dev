be3fd31b1b7a04583886f3976e251371546f81fb
Sawayaka <eskenoval14@gmail.com>
2026-02-17 23:05:52 +0500

    Lab4

---
+++ Lab4/Task2/online-store/src/app/app.ts
@@ -0,0 +1,70 @@
+import { Component, signal } from '@angular/core';
+import { ProductList } from './components/product-list/product-list';
+import { Header } from './components/header/header';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [Header, RouterModule, ProductList],
+  template: `
+    <app-header></app-header>
+
+    <div class='main-container'>
+      <aside class='sidebar'>
+        <h3>Все категории</h3>
+        <ul>
+          <li><a routerLink="/products">Все товары</a></li>
+          <li><a routerLink="/products" [queryParams]="{category: 'Phones'}">Телефоны и гаджеты</a></li>
+          </ul>
+      </aside>
+
+      <main class="content">
+        <router-outlet></router-outlet>
+      </main>
+    </div>
+  `,
+styles: [`
+    .main-container {
+      display: flex;
+      gap: 20px;
+      padding: 20px;
+      max-width: 1200px;
+      margin: 0 auto;
+    }
+
+    .sidebar {
+      width: 250px;
+      flex-shrink: 0;
+      background: #f5f5f5;
+      padding: 15px;
+      border-radius: 8px;
+      height: fit-content;
+    }
+
+    .sidebar ul {
+      list-style: none;
+      padding: 0;
+    }
+
+    .sidebar li {
+      margin-bottom: 10px;
+    }
+
+    .sidebar a {
+      text-decoration: none;
+      color: #333;
+      font-size: 14px;
+    }
+
+    .sidebar a:hover {
+      color: #d32f2f;
+    }
+
+    .content {
+      flex-grow: 1;
+    }
+  `]
+})
+export class App {
+  protected readonly title = signal('Kaspi');
+}
