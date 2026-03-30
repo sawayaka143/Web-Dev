b514f6c85b2760f805732b24ab3a42cc74726f43
Sawayaka <eskenoval14@gmail.com>
2026-03-30 16:37:25 +0500

    a

--- Lab8/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductList } from './components/product-list/product-list';
-
-export const routes: Routes = [
-  {path: '', component: ProductList},
-];
